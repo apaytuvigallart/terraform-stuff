35299cfa8574ab02c647e277d110dcca65dee668
apaytuvigallart <aleixpaytuvi@gmail.com>
2025-06-27 14:25:04 +0200

    feat: create s3 bucket and store state in existing s3

---
+++ 04-backends/s3.tf
@@ -0,0 +1,15 @@
+resource "random_id" "bucket_id" {
+  byte_length = 12
+  prefix      = "tf-bucket-"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = random_id.bucket_id.hex
+  tags = {
+    Name = "Terraform S3 Bucket D:"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
